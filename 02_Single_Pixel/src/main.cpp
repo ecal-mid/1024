1c7e9116227c1c04eb38b4727e773dff6c623db5
Andreas Gysin <andreas@ertdfgcvb.ch>
2023-10-02 16:05:02 +0200

    Update main.cpp

--- 02_Single_Pixel/src/main.cpp
+++ 02_Single_Pixel/src/main.cpp
@@ -1,11 +1,7 @@
 #include <Arduino.h>
 
-// PICO
-#include "pico_driver_v5_pinout.h"
-
 // TEENSY
-// #include <MatrixHardware_Teensy4_ShieldV5.h>
-
+#include <MatrixHardware_Teensy4_ShieldV5.h>
 #include <SmartMatrix.h>
 
 #define COLOR_DEPTH 24
@@ -15,11 +11,10 @@ const uint16_t TOTAL_HEIGHT = 32;
 const uint16_t kRefreshDepth = 48;  // Valid: 24, 36, 48
 const uint16_t kDmaBufferRows = 4;  // Valid: 2-4
 
-// const uint16_t kPanelType = SM_PANELTYPE_HUB75_64ROW_MOD32SCAN;
-
+//const uint16_t kPanelType = SM_PANELTYPE_HUB75_64ROW_MOD32SCAN;
 const uint16_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;
 
-const uint16_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE | SM_HUB75_OPTIONS_FM6126A_RESET_AT_START);
+const uint16_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);
 // const uint16_t kMatrixOptions = (SM_HUB75_OPTIONS_ESP32_INVERT_CLK);
 // const uint16_t kMatrixOptions = (SM_HUB75_OPTIONS_FM6126A_RESET_AT_START);
 
@@ -30,9 +25,6 @@ uint16_t getMaxValue(uint8_t *arr, uint16_t len);
 SMARTMATRIX_ALLOCATE_BUFFERS(matrix, TOTAL_WIDTH, TOTAL_HEIGHT, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
 SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(bg, TOTAL_WIDTH, TOTAL_HEIGHT, COLOR_DEPTH, kbgOptions);
 
-rgb24 WHITE = rgb24(255, 255, 255);
-rgb24 BLACK = rgb24(0, 0, 0);
-
 void setup() {
 
 	//pinMode(PICO_LED_PIN, OUTPUT);
@@ -41,15 +33,18 @@ void setup() {
 	matrix.addLayer(&bg);
 	matrix.setBrightness(255);
 	matrix.begin();
-	bg.fillScreen(BLACK);
 
 }
 
 uint32_t frame = 0;
 void loop() {
-	bg.fillScreen(rgb24(0,0,0));
+	bg.fillScreen( {255,0,0});
+
+
 	bg.drawPixel(0, frame % 32, {255, 0, 0});
-	bg.swapBuffers(false);
+
+
+	bg.swapBuffers(true);
 
 	frame++;
 	delay(100);
